bd811d65b40e96af1c3314addb32c1586ecc123b
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-08-29 11:10:13 -0300

    Header Responsivo terminado

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,19 +1,23 @@
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 
 export function Header() {
     return (
-        
-        <header className="bg-[#1F6482] text-white p-4 flex justify-between items-center" style={{ fontFamily: "'Noto Sans', sans-serif" }}>
-            <img src="/UANE.svg" alt="Logo UANE" className="h-10" />
-            <nav className="hidden md:flex space-x-6">
-                <a href="#courses" className="hover:text-gray-300 font-bold">CURSOS</a>
-                <a href="#teachers" className="hover:text-gray-300 font-bold">PROFESSORES</a>
-                <a href="#about" className="hover:text-gray-300 font-bold">SOBRE</a>
-                <a href="#partners" className="hover:text-gray-300 font-bold">PARCERIAS</a>
+
+        <header className="bg-[#1F6482] text-white py-[16px] flex justify-between items-center lg:py-[24px] " style={{ fontFamily: "'Noto Sans', sans-serif" }}>
+            <img src="src/assets/UANE.svg" alt="Logo UANE" className="w-[75px] h-[24px] ml-[12px] lg:ml-[72px]" />
+            <nav className="hidden md:flex space-x-8">
+                <a href="#courses" className="hover:text-gray-300 font-bold text-sm">CURSOS</a>
+                <a href="#teachers" className="hover:text-gray-300 font-bold text-sm">PROFESSORES</a>
+                <a href="#about" className="hover:text-gray-300 font-bold text-sm">SOBRE A UANE</a>
+                <a href="#partners" className="hover:text-gray-300 font-bold text-sm">PARCERIAS</a>
+                <a href="#portal" className="flex items-center gap-2 hover:text-gray-300 font-bold text-sm mr-[72px]">
+                    <i className="fa-solid fa-user-circle text-xl"></i>
+                    PORTAL DO ALUNO
+                </a>
             </nav>
-            <button className="md:hidden">☰</button>
+            <button className=" mr-[12px] md:hidden">☰</button>
         </header>
     );
 }
