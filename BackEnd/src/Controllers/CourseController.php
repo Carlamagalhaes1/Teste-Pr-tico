c0b0cb0bbfbef38cb2f23d14f38e3d997cb06513
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-08-31 21:42:34 -0300

    Finalizado projeto e entregue doc/readme

--- BackEnd/src/Controllers/CourseController.php
+++ BackEnd/src/Controllers/CourseController.php
@@ -6,63 +6,64 @@ use App\Http\Response;
 
 class CourseController {
     private array $courses = [
-        [
-            "id" => 1,
-            "tipo" => ["EXTENSÃO", "BEM-VIVER"],
-            "titulo" => "Educação Ambiental para um Presente Sustentável",
-            "autor" => "Fundação Demócrito Rocha",
-            "imagem" => "https://picsum.photos/400/250?random=1"
-        ],
-        [
-            "id" => 2,
-            "tipo" => ["CURTA DURAÇÃO", "TECNOLOGIA E PROFISSÃO"],
-            "titulo" => "Como Implementar a Política Municipal de Educação Ambiental",
-            "autor" => "Daniel Pagliuca",
-            "imagem" => "https://picsum.photos/400/250?random=2"
-        ],
-        [
-            "id" => 3,
-            "tipo" => ["CURTA DURAÇÃO", "BEM-VIVER"],
-            "titulo" => "Bullying: conhecer para combater, como evitar.",
-            "autor" => "Grecianny Carvalho",
-            "imagem" => "https://picsum.photos/400/250?random=3"
-        ],
-        [
-            "id" => 4,
-            "tipo" => ["EXTENSÃO", "BEM-VIVER"],
-            "titulo" => "Harmonização Tributária Intergovernamental",
-            "autor" => "Fundação Demócrito Rocha",
-            "imagem" => "https://picsum.photos/400/250?random=4"
-        ],
-        [
-            "id" => 5,
-            "tipo" => ["CURTA DURAÇÃO", "SAÚDE"],
-            "titulo" => "Nutrição e Qualidade de Vida, melhores práticas.",
-            "autor" => "Ana Paula Lima",
-            "imagem" => "https://picsum.photos/400/250?random=5"
-        ],
-        [
-            "id" => 6,
-            "tipo" => ["EXTENSÃO", "TECNOLOGIA"],
-            "titulo" => "Introdução ao Desenvolvimento Web.",
-            "autor" => "João Silva",
-            "imagem" => "https://picsum.photos/400/250?random=6"
-        ],
-        [
-            "id" => 7,
-            "tipo" => ["CURTA DURAÇÃO", "BEM-VIVER"],
-            "titulo" => "Atenção Plena e Qualidade de Vida, cuidando-se.",
-            "autor" => "Maria Oliveira",
-            "imagem" => "https://picsum.photos/400/250?random=7"
-        ],
-        [
-            "id" => 8,
-            "tipo" => ["EXTENSÃO", "TECNOLOGIA E PROFISSÃO"],
-            "titulo" => "Inteligência Artificial no Dia a Dia, como entender.",
-            "autor" => "Carlos Souza",
-            "imagem" => "https://picsum.photos/400/250?random=8"
-        ]
-    ];
+    [
+        "id" => 1,
+        "tipo" => ["EXTENSÃO", "BEM-VIVER"],
+        "titulo" => "Educação Ambiental para um Presente Sustentável",
+        "autor" => "Fundação Demócrito Rocha",
+        "imagem" => "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60" 
+    ],
+    [
+        "id" => 2,
+        "tipo" => ["CURTA DURAÇÃO", "TECNOLOGIA E PROFISSÃO"],
+        "titulo" => "Como Implementar a Política Municipal de Educação Ambiental",
+        "autor" => "Daniel Pagliuca",
+        "imagem" => "https://img.freepik.com/premium-photo/green-city-skyline-through-trees_57163-4360.jpg?w=2000" 
+    ],
+     [
+    "id" => 3,
+    "tipo" => ["CURTA DURAÇÃO", "BEM-VIVER"],
+    "titulo" => "Bullying: conhecer para combater, como evitar.",
+    "autor" => "Grecianny Carvalho",
+    "imagem" => "https://plus.unsplash.com/premium_photo-1661380704283-38adf12c3f5e?fm=jpg&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YnVsbHlpbmd8ZW58MHx8MHx8fDA%3D&ixlib=rb-4.0.3&q=60&w=3000"
+     ],
+    [
+        "id" => 4,
+        "tipo" => ["EXTENSÃO", "BEM-VIVER"],
+        "titulo" => "Harmonização Tributária Intergovernamental",
+        "autor" => "Fundação Demócrito Rocha",
+        "imagem" => "https://images.unsplash.com/photo-1554224154-22dec7ec8818?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60" 
+    ],
+    [
+        "id" => 5,
+        "tipo" => ["CURTA DURAÇÃO", "SAÚDE"],
+        "titulo" => "Nutrição e Qualidade de Vida, melhores práticas.",
+        "autor" => "Ana Paula Lima",
+        "imagem" => "https://images.unsplash.com/photo-1506806732259-39c2d0268443?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60" 
+    ],
+    [
+        "id" => 6,
+        "tipo" => ["EXTENSÃO", "TECNOLOGIA"],
+        "titulo" => "Introdução ao Desenvolvimento Web.",
+        "autor" => "João Silva",
+        "imagem" => "https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60" 
+    ],
+    [
+        "id" => 7,
+        "tipo" => ["CURTA DURAÇÃO", "BEM-VIVER"],
+        "titulo" => "Atenção Plena e Qualidade de Vida, cuidando-se.",
+        "autor" => "Maria Oliveira",
+        "imagem" => "https://images.unsplash.com/photo-1506126613408-eca07ce68773?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60" 
+    ],
+    [
+        "id" => 8,
+        "tipo" => ["EXTENSÃO", "TECNOLOGIA E PROFISSÃO"],
+        "titulo" => "Inteligência Artificial no Dia a Dia, como entender.",
+        "autor" => "Carlos Souza",
+        "imagem" => "https://images.unsplash.com/photo-1677442136019-21780ecad995?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60" 
+    ]
+];
+
 
     public function index(Request $req, Response $res): void {
         $res->json($this->courses);
--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,20 +1,92 @@
+import { useState } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Menu, X } from "lucide-react";
 
 export default function Header() {
-    return (
-        <header className="bg-[#1F6482] text-white py-[16px] flex justify-between items-center lg:py-[24px] " style={{ fontFamily: "'Noto Sans', sans-serif" }}>
-            <img src="src/assets/UANE.svg" alt="Logo UANE" className="w-[75px] h-[24px] ml-[12px] lg:ml-[72px]" />
-            <nav className="hidden lg:flex space-x-8">
-                <a href="/courses" className="hover:text-gray-300 font-bold text-xs">CURSOS</a>
-                <a href="#teachers" className="hover:text-gray-300 font-bold text-xs">PROFESSORES</a>
-                <a href="#about" className="hover:text-gray-300 font-bold text-xs">SOBRE A UANE</a>
-                <a href="#partners" className="hover:text-gray-300 font-bold text-xs">PARCERIAS</a>
-                <a href="#portal" className="flex items-center gap-2 hover:text-gray-300 font-bold text-xs mr-[72px]">
-                    <i className="fa-solid fa-user-circle text-sm"></i>
-                    PORTAL DO ALUNO
-                </a>
-            </nav>
-            <button className=" mr-[12px]  lg:hidden">☰</button>
-        </header>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header
+      className="bg-[#1F6482] text-white py-[16px] flex justify-between items-center lg:py-[24px]" style={{ fontFamily: "'Noto Sans', sans-serif" }}>
+      <img
+        src="src/assets/UANE.svg"
+        alt="Logo UANE"
+        className="w-[75px] h-[24px] ml-[12px] lg:ml-[72px]"
+      />
+
+     
+      <nav className="hidden lg:flex space-x-8">
+        <a href="#courses" className="hover:text-gray-300 font-bold text-xs">
+          CURSOS
+        </a>
+        <a href="#teachers" className="hover:text-gray-300 font-bold text-xs">
+          PROFESSORES
+        </a>
+        <a href="#about" className="hover:text-gray-300 font-bold text-xs">
+          SOBRE A UANE
+        </a>
+        <a href="#partners" className="hover:text-gray-300 font-bold text-xs">
+          PARCERIAS
+        </a>
+        <a
+          href="#portal"
+          className="flex items-center gap-2 hover:text-gray-300 font-bold text-xs mr-[72px]"
+        >
+          <i className="fa-solid fa-user-circle text-sm"></i>
+          PORTAL DO ALUNO
+        </a>
+      </nav>
+
+      
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="mr-[12px] lg:hidden p-2"
+        aria-label="Menu"
+      >
+        {isOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
+      </button>
+
+  
+      {isOpen && (
+        <div className="absolute top-[70px] right-3 w-[220px] rounded-2xl bg-white/90 backdrop-blur-md shadow-xl p-4 flex flex-col space-y-4 text-[#1F6482] z-50 animate-fadeIn">
+          <a
+            href="#courses"
+            className="hover:text-[#145265] font-semibold text-sm"
+            onClick={() => setIsOpen(false)}
+          >
+            CURSOS
+          </a>
+          <a
+            href="#teachers"
+            className="hover:text-[#145265] font-semibold text-sm"
+            onClick={() => setIsOpen(false)}
+          >
+            PROFESSORES
+          </a>
+          <a
+            href="#about"
+            className="hover:text-[#145265] font-semibold text-sm"
+            onClick={() => setIsOpen(false)}
+          >
+            SOBRE A UANE
+          </a>
+          <a
+            href="#partners"
+            className="hover:text-[#145265] font-semibold text-sm"
+            onClick={() => setIsOpen(false)}
+          >
+            PARCERIAS
+          </a>
+          <a
+            href="#portal"
+            className="flex items-center gap-2 hover:text-[#145265] font-semibold text-sm"
+            onClick={() => setIsOpen(false)}
+          >
+            <i className="fa-solid fa-user-circle text-sm"></i>
+            PORTAL DO ALUNO
+          </a>
+        </div>
+      )}
+    </header>
+  );
 }
